a9bf5f65581a1ef0d1aa3db65d2d19e184b8eb95
Rescor <rescorent@gmail.com>
2022-08-19 21:50:43 +0200

    Fixed galaxy change

--- includes/pages/game/ShowGalaxyPage.class.php
+++ includes/pages/game/ShowGalaxyPage.class.php
@@ -91,7 +91,7 @@ class ShowGalaxyPage extends AbstractGamePage
         $this->tplObj->loadscript('galaxy.js');
         $this->assign(array(
 			'GalaxyRows'				=> $Result,
-			'planetcount'				=> sprintf($LNG['gl_populed_planets'], count($Result)),
+			'planetcount'				=> sprintf($LNG['gl_populed_planets'], isset($Result) ? count($Result) : 0),
 			'action'					=> $action,
 			'galaxy'					=> $galaxy,
 			'system'					=> $system,
